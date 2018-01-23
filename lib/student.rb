22c6c3833845522f36677a8cc3d9b966c5092eed
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-01-23 19:42:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,4 +96,15 @@ class Student
 
     DB[:conn].execute(sql, x)
   end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade
+    IS 10
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
 end
